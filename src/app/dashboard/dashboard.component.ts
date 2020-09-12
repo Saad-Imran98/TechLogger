60d0701a4fa98af8340df2f967e023b3447af864
saad2379 <saad2379@gmail.com>
2020-09-12 11:43:42 +0100

    Home fade in animation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Issue} from '../../Issue';
 import {IssueService} from '../issue.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  animations: [
+    trigger('fade',[
+
+      transition('void=> *', [
+        style({backgroundColor: '', opacity: 0}),
+        animate(500)
+      ]),
+
+      transition('* => void', [
+        animate(2000, style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
 
